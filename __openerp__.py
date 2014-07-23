75c22a9efc8ed3b3befd32f60ab6b1a23668ea07
chengdh <cheng.donghui@gmail.com>
2014-07-23 21:35:36 +0800

    first commit

---
+++ __openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'custom security for ktv',
+    'version': '0.1',
+    'category': 'Uncategorized',
+    'description': """定制权限,包括角色、访问权限、菜单等""",
+    'author': 'chengdh (cheng.donghui@gmail.com)',
+    'website': '',
+    'license': 'AGPL-3',
+    'depends': [],
+    'data': [
+      "menu_view.xml",
+      "security/custom_security.xml",
+      "security/ir.model.access.csv",
+      ],
+    'demo_xml': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'web':True,
+    'css': [],
+    'js': [],
+    'xml': [],
+}
